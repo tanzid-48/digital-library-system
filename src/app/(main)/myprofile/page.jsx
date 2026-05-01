87c579f42cce668ac265c1b0dec45ae4a363e263
tanzid-48 <mdtanzid.525@gmail.com>
2026-05-01 10:00:39 +0600

    Set Butter-auth and mongoDB database

--- src/app/(main)/myprofile/page.jsx
+++ src/app/(main)/myprofile/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ProfilePage = () => {
-    return (
-        <div>
-            <h2>Visit My Profile</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>updated you profile</h2>
+    </div>
+  );
 };
 
-export default ProfilePage
+export default ProfilePage;
---
+++ src/app/(main)/myprofile/update/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdatedPage = () => {
+    return (
+        <div>
+            <h2>Updated Page</h2>
+        </div>
+    );
+};
+
+export default UpdatedPage;
